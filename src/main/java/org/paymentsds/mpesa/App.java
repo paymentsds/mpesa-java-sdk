9ae107c2a75cd06eafcdbfec46b21c0fa9fb9504
Elton Laice <me@eltonlaice.com>
2020-09-14 04:23:15 +0200

    first commit

---
+++ src/main/java/org/paymentsds/mpesa/App.java
@@ -0,0 +1,13 @@
+package org.paymentsds.mpesa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
